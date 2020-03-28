d76c130cb899dc5185a4ee3bc08eb371fc201382
Rayra <rayramariana_live@hotmail.com>
2020-03-28 18:43:42 -0300

    Adicionando validação e testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,6 +29,7 @@
  * Query Builder: table(users).select('*').where
 */
 const express = require('express');
+const {errors} = require('celebrate');
 const cors = require('cors');
 const routes = require('./routes');
 
@@ -40,5 +41,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333);
+module.exports=app;
